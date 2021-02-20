30ed254fc6b3cefd20863303f4e0cd5c4ab9057f
franck gaspoz <franck.gaspoz@gmail.com>
2021-02-20 23:09:13 +0100

    update-push

--- publish-nuget.sh
+++ publish-nuget.sh
@@ -1,3 +1,3 @@
 ﻿#!orbsh
 # publish package on nuget
-nuget-push bin/debug/netcoreapp3.1/OrbitalShell-Module-DoomFireAlgo.1.0.0.nupkg $key
+nuget-push bin/debug/OrbitalShell-Module-DoomFireAlgo.1.0.0.nupkg $key
